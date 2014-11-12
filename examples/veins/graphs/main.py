876e1cea3432b0445996847e43431ae33619b4ea
DankilltheShadow <mulato.daniel@gmail.com>
2014-11-12 11:55:39 +0100

    primo graph in python

--- examples/veins/graphs/main.py
+++ examples/veins/graphs/main.py
@@ -1,12 +1,29 @@
 __author__ = 'Daniel'
 
 import pandas as pd
+import matplotlib.pyplot as plt
 
 def graph():
     df = pd.read_csv('..\\results\\nodebug-0.csv', header=0)
-    df.rename(columns=lambda x: x.replace('RSUExampleScenario.node','Car'), inplace=True)
-    df.rename(columns=lambda x: x.replace('appl/',''), inplace=True)
+    df.rename(columns=lambda x: x.replace('RSUExampleScenario.node', 'Car'), inplace=True)
+    df.rename(columns=lambda x: x.replace('appl/', ''), inplace=True)
 
-    print df
+    chchannels = df.filter(regex="CH state")
+    chsum = chchannels.sum().sum()
+    onchannels = df.filter(regex="ON state")
+    onsum = onchannels.sum().sum()
+    fnchannels = df.filter(regex="FN state")
+    fnsum = fnchannels.sum().sum()
+
+    labels = 'FN', 'ON', 'CH'
+    sizes = [fnsum, onsum, chsum]
+    colors = ['yellowgreen', 'lightskyblue', 'lightcoral']
+    explode = (0, 0, 0)
+
+    plt.pie(sizes, explode=explode, labels=labels, colors=colors, autopct='%1.1f%%', shadow=True, startangle=90)
+
+    plt.axis('equal')
+
+    plt.show()
 
 graph()
