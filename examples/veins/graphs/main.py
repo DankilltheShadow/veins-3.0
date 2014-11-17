2660a08d42b53b8ac056540396c5f041409057df
DankilltheShadow <mulato.daniel@gmail.com>
2014-11-17 16:21:01 +0100

    pie migliorato

--- examples/veins/graphs/main.py
+++ examples/veins/graphs/main.py
@@ -5,7 +5,6 @@ import math
 import matplotlib.pyplot as plt
 import pylab
 import time as tm
-pylab.ion()
 
 ###importazione dei dati######################
 df = pd.read_csv('..\\results\\nodebug-0.csv', header=0)
@@ -19,33 +18,54 @@ onstates = df.filter(regex="ON state")
 fnstates = df.filter(regex="FN state")
 ##############################################
 
-
-def get_fig(some_data, some_labels, title):
-    ax = fig.add_subplot(111)
-    ax.set_ylim([0.1, 0.8])
-    ax.set_xlim([0.1, 0.8])
-    ax.set_title(title)
-    ax.pie(some_data, labels=some_labels, autopct='%1.1f%%', shadow=False, startangle=90)
-
-my_labels = 'FN', 'ON', 'CH'
-fig = pylab.figure(figsize=(8, 8), frameon=False)
+figure = plt.figure()
+# plotting
 chsum = 0.0
 fnsum = 0.0
 onsum = 0.0
 
 for i in range(0, len(time.index)):
     if math.fmod(time.iloc[i].sum(), (beaconinterval/2)) == 0:
-        fig.clear()
-        title = str(time.iloc[i].sum())
         chsum += chstates.iloc[i].sum()
         onsum += onstates.iloc[i].sum()
-        fnsum += onstates.iloc[i].sum()
+        fnsum += fnstates.iloc[i].sum()
 
-        data = [fnsum, onsum, chsum]
+        index = i
+        break
 
-        get_fig(data, my_labels, title)
-        fig.canvas.draw()
-        pylab.draw()
-        tm.sleep(1)
+plt.pie([fnsum, onsum, chsum], labels=['FN', 'ON', 'CH'], autopct='%1.1f%%', shadow=False, startangle=90)
 
-pylab.show()
+
+def on_press(event):
+    global index
+    global chsum
+    global onsum
+    global fnsum
+    if event.key == u'right':
+        event.canvas.figure.clear()
+        for c in range(index+1, len(time.index)):
+            if math.fmod(time.iloc[c].sum(), (beaconinterval/2)) == 0:
+                chsum += chstates.iloc[c].sum()
+                onsum += onstates.iloc[c].sum()
+                fnsum += fnstates.iloc[c].sum()
+
+                index = c
+                break
+        event.canvas.figure.gca().pie([fnsum, onsum, chsum], labels=['FN', 'ON', 'CH'], autopct='%1.1f%%', shadow=False, startangle=90)
+        event.canvas.draw()
+    else:
+        if event.key == u'left':
+            event.canvas.figure.clear()
+            for c in range(index, -1, -1):
+                if math.fmod(time.iloc[c].sum(), (beaconinterval/2)) == 0:
+                    chsum -= chstates.iloc[c].sum()
+                    onsum -= onstates.iloc[c].sum()
+                    fnsum -= fnstates.iloc[c].sum()
+
+                    index = c-1
+                    break
+            event.canvas.figure.gca().pie([fnsum, onsum, chsum], labels=['FN', 'ON', 'CH'], autopct='%1.1f%%', shadow=False, startangle=90)
+            event.canvas.draw()
+
+figure.canvas.mpl_connect('key_press_event', on_press)
+plt.show()
