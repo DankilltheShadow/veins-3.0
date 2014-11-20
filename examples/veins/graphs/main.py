9525076df28076b116131b1f86825c26491e6e2e
DankilltheShadow <mulato.daniel@gmail.com>
2014-11-20 11:25:00 +0100

    py

--- examples/veins/graphs/main.py
+++ examples/veins/graphs/main.py
@@ -15,56 +15,51 @@ time = df.filter(regex="X")
 chstates = df.filter(regex="CH state")
 onstates = df.filter(regex="ON state")
 fnstates = df.filter(regex="FN state")
+statestime_index = time.select(lambda x: math.fmod(time.iloc[x].sum(), (beaconinterval/2)) == 0).index.tolist()
+xcoord = df.filter(regex="Posizione x")
+ycoord = df.filter(regex="Posizione y")
+xcoordheat = xcoord.select(lambda x: x not in statestime_index)
+ycoordheat = ycoord.select(lambda x: x not in statestime_index)
+xcoordheat = [tuple(x) for x in xcoordheat.values]
+ycoordheat = [tuple(x) for x in ycoordheat.values]
 ##############################################
-
-figure = plt.figure()
-# plotting
-chsum = 0.0
-fnsum = 0.0
-onsum = 0.0
-
-for i in range(0, len(time.index)):
-    if math.fmod(time.iloc[i].sum(), (beaconinterval/2)) == 0:
-        chsum += chstates.iloc[i].sum()
-        onsum += onstates.iloc[i].sum()
-        fnsum += fnstates.iloc[i].sum()
-
-        index = i
-        break
-
-plt.pie([fnsum, onsum, chsum], labels=['FN', 'ON', 'CH'], autopct='%1.1f%%', shadow=False, startangle=90)
-
-
-def on_press(event):
-    global index
-    global chsum
-    global onsum
-    global fnsum
-    if event.key == u'right':
-        event.canvas.figure.clear()
-        for c in range(index+1, len(time.index)):
-            if math.fmod(time.iloc[c].sum(), (beaconinterval/2)) == 0:
-                chsum += chstates.iloc[c].sum()
-                onsum += onstates.iloc[c].sum()
-                fnsum += fnstates.iloc[c].sum()
-
-                index = c
-                break
-        event.canvas.figure.gca().pie([fnsum, onsum, chsum], labels=['FN', 'ON', 'CH'], autopct='%1.1f%%', shadow=False, startangle=90)
-        event.canvas.draw()
-    else:
-        if event.key == u'left':
-            event.canvas.figure.clear()
-            for c in range(index, -1, -1):
-                if math.fmod(time.iloc[c].sum(), (beaconinterval/2)) == 0:
-                    chsum -= chstates.iloc[c].sum()
-                    onsum -= onstates.iloc[c].sum()
-                    fnsum -= fnstates.iloc[c].sum()
-
-                    index = c-1
-                    break
-            event.canvas.figure.gca().pie([fnsum, onsum, chsum], labels=['FN', 'ON', 'CH'], autopct='%1.1f%%', shadow=False, startangle=90)
-            event.canvas.draw()
-
-figure.canvas.mpl_connect('key_press_event', on_press)
+plt.hist2d(xcoordheat, ycoordheat, cmap='Greens')
+# figure = plt.figure()
+# id = 0
+# chsum = chstates.iloc[statestime_index[id]].sum()
+# onsum = onstates.iloc[statestime_index[id]].sum()
+# fnsum = fnstates.iloc[statestime_index[id]].sum()
+#
+# plt.pie([fnsum, onsum, chsum], labels=['FN', 'ON', 'CH'], autopct='%1.1f%%', shadow=False, startangle=90)
+#
+#
+# def on_press(event):
+#     global id
+#     global chsum
+#     global onsum
+#     global fnsum
+#
+#     if event.key == u'right' and id < len(statestime_index)-1:
+#         event.canvas.figure.clear()
+#
+#         id += 1
+#         chsum += chstates.iloc[statestime_index[id]].sum()
+#         onsum += onstates.iloc[statestime_index[id]].sum()
+#         fnsum += fnstates.iloc[statestime_index[id]].sum()
+#
+#         event.canvas.figure.gca().pie([fnsum, onsum, chsum], labels=['FN', 'ON', 'CH'], autopct='%1.1f%%', shadow=False, startangle=90)
+#         event.canvas.draw()
+#     else:
+#         if event.key == u'left' and id >= 0:
+#             event.canvas.figure.clear()
+#
+#             chsum -= chstates.iloc[statestime_index[id]].sum()
+#             onsum -= onstates.iloc[statestime_index[id]].sum()
+#             fnsum -= fnstates.iloc[statestime_index[id]].sum()
+#             id -= 1
+#
+#             event.canvas.figure.gca().pie([fnsum, onsum, chsum], labels=['FN', 'ON', 'CH'], autopct='%1.1f%%', shadow=False, startangle=90)
+#             event.canvas.draw()
+#
+# figure.canvas.mpl_connect('key_press_event', on_press)
 plt.show()
